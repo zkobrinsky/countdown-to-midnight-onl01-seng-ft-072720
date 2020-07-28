148d778602bac4b58896c3ee300716c21d31f939
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-28 16:40:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(number)
-  counter = number
-  while counter > 0
-    counter -= 1
+  
+  while number > 0
+    number -= 1
     "#{number} SECOND(S)!"
   end
   "HAPPY NEW YEAR!"
