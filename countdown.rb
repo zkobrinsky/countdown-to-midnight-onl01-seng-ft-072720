dd728c2f6e0138b63dd654ee56b96e0925551266
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-28 16:46:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,15 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(number)
+  counter = number
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
+
